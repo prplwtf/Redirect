9564af35df6d88ad4f9f191fe62e0f35b51d374a
purple <hello@prpl.wtf>
2023-06-28 19:40:03 +0200

    .blueprint folder structure has changed.

--- data/install.sh
+++ data/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm ^#datapath#^/storage/redirects/hello.txt;
-chown -R www-data:www-data /var/www/pterodactyl/.blueprint/.storage/extensiondata/redirect/*;
+chown -R www-data:www-data /var/www/pterodactyl/.blueprint/data/extensions/redirect/*;
